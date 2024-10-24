0e417364e07e12d226827a501503e38e98d9c39d
BNK4970 <enzocordo66@gmail.com>
2024-10-24 11:00:42 +0200

    ajout à la rache avant de clone la branche test sur la branch main

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -2,9 +2,9 @@ import Button from "@ui/button";
 import React from "react";
 export default function Home() {
   return (
-      <React.Fragment>
+      <>
         <p>Hello,World!</p>
-        <Button>Coucou</Button>
-      </React.Fragment>
+        <Button primary={true}>Coucou</Button>
+      </>
   );
 }
